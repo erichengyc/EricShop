0f8c52dd36784379eda5d443d6390d671d5787eb
erichengyc <eheng57@gmail.com>
2020-08-29 17:46:47 +0930

    add EF, DbContext, Connection String

---
+++ EricShop/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EricShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+            base(options)
+        {
+        }
+
+        public DbSet<Aesop> Aesops { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
