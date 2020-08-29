d6b0d46e7e14856da81bcefc246d85ac03d46d7c
erichengyc <eheng57@gmail.com>
2020-08-29 22:09:10 +0930

    Creating partial views, home controller and view

---
+++ EricShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EricShop.Models;
+using EricShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EricShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAesopRepository _aesopRepository;
+
+        public HomeController(IAesopRepository aesopRepository)
+        {
+            _aesopRepository = aesopRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                AesopOnSale = _aesopRepository.GetAesopOnSale
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ EricShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EricShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EricShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Aesop> AesopOnSale { get; set; }
+    }
+}
