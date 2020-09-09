d12b037f2a4daed8e7b88ff9aca32463068be78c
erichengyc <eheng57@gmail.com>
2020-09-09 18:38:37 +0930

    Fixing issues order table username column

---
+++ EricShop/Migrations/20200909090417_Username1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EricShop.Migrations
+{
+    public partial class Username1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Aesops",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Aesops");
+        }
+    }
+}
